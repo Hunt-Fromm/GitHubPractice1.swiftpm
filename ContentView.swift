9bf32a30f22bc1ef339264c33f48d936be9cc588
Hunter Frommelt <fro512131@d204.lths.net>
2025-04-08 12:08:00 -0500

    Changed answer to Int

--- ContentView.swift
+++ ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     
     @State var number1: Int = 0
     @State var number2 = 2
-    @State var answer = ""
+    @State var answer = 0
 
     
     var body: some View {
